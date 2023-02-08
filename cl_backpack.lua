890e62b3292ffd7a7d2bcc8faea0fed6cae165d1
FRANKOx69 <74464162+FRx69@users.noreply.github.com>
2023-02-08 16:01:07 +0100

    Update cl_backpack.lua

--- cl_backpack.lua
+++ cl_backpack.lua
@@ -1,19 +1,28 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('cl_backpack:client:small_backpack', function()
-    TriggerEvent('animations:client:EmoteCommandStart', {"backpack"})
+    TriggerEvent('animations:client:EmoteCommandStart', {"kneel3"})
     QBCore.Functions.Progressbar("Opening Small BackPack", "Opening Small BackPack..", 5000, false, true, {
         disableMovement = false,
         disableCarMovement = false,
 		disableMouse = false,
 		disableCombat = true,
     }, {}, {}, {}, function() -- Done
-        TriggerEvent('animations:client:EmoteCommandStart', {"backpack"})
-        TriggerEvent("inventory:client:SetCurrentStash", "SL_637883")
-        TriggerServerEvent("inventory:server:OpenInventory", "stash", "SL_637883", {
-            maxweight = 4000000,
-            slots = 10,
+        local CID = QBCore.Functions.GetPlayerData().citizenid
+        TriggerServerEvent("inventory:server:OpenInventory", "stash", "BackPack" .. CID, {
+            maxweight = 100000,
+            slots = 5,
         })
+        TriggerEvent("inventory:client:SetCurrentStash", "BackPack" .. CID)
+        local backpackData = {
+            outfitData = {
+                ["bag"]   = { item = 56, texture = 6},  
+            }
+        }
+        TriggerEvent('qb-clothing:client:loadOutfit', backpackData)
+        backpackEquiped = true
+        TaskPlayAnim(ped, "clothingshirt", "exit", 8.0, 1.0, -1, 49, 0, 0, 0, 0)
+        TriggerEvent('animations:client:EmoteCommandStart', {"shakeoff"})
     end)
 end)
 
@@ -26,10 +35,11 @@ RegisterNetEvent('cl_backpack:client:suitcase2', function()
 		disableCombat = true,
     }, {}, {}, {}, function() -- Done
         TriggerEvent('animations:client:EmoteCommandStart', {"suitcase2"})
-        TriggerEvent("inventory:client:SetCurrentStash", "SL_637883")
-        TriggerServerEvent("inventory:server:OpenInventory", "stash", "SL_637883", {
-            maxweight = 4000000,
-            slots = 100,
+        local CID = QBCore.Functions.GetPlayerData().citizenid
+        TriggerServerEvent("inventory:server:OpenInventory", "stash", "SuitCase" .. CID, {
+            maxweight = 300000,
+            slots = 10,
         })
+        TriggerEvent("inventory:client:SetCurrentStash", "SuitCase" .. CID)
     end)
 end)
